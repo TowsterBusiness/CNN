c488f00227bf814004386397e56960ecc79f8f59
towster <towster.business@gmail.com>
2024-07-05 03:31:33 -0700

    PoolingLayer FeedForward

--- src/main/java/me/Towster/Layers/PoolingLayer.java
+++ src/main/java/me/Towster/Layers/PoolingLayer.java
@@ -10,7 +10,7 @@ public class PoolingLayer extends Layer {
     int filterHeight;
     int depth;
 
-    public PoolingLayer(int filterWidth, int filterHeight, int depth, int stepSize) {
+    public PoolingLayer(int filterWidth, int filterHeight, int depth) {
         this.filterWidth = filterWidth;
         this.filterHeight = filterHeight;
         this.depth = depth;
@@ -36,7 +36,30 @@ public class PoolingLayer extends Layer {
 
     @Override
     public List<double[][]> feedForward(List<double[][]> dataIn) {
-        return List.of();
+        List<double[][]> outData = new ArrayList<>();
+        for (int dataInPointer = 0; dataInPointer < dataIn.size(); dataInPointer++) {
+            double[][] dataInXY = dataIn.get(dataInPointer);
+            double[][] outXY = new double[dataInXY[0].length - filterHeight][dataInXY.length - filterHeight];
+            for (int yPointer = 0; yPointer < dataInXY[0].length - filterHeight; yPointer ++) {
+                for (int xPointer = 0; xPointer < dataInXY.length - filterHeight; xPointer ++) {
+                    double outSum = 0;
+                    for (int filterY = 0; filterY < filterHeight; filterY++) {
+                        for (int filterX = 0; filterX < filterWidth; filterX++) {
+                            outSum += dataInXY[yPointer + filterY][xPointer + filterX]
+                                    * weights.get(dataInPointer)[filterY][filterX];
+                        }
+                    }
+                    outSum /= filterHeight * filterWidth;
+                    outXY[yPointer][xPointer] = ReLU(outSum);
+                }
+            }
+            outData.add(outXY);
+        }
+
+        if (_nextLayer != null) {
+            return _nextLayer.feedForward(outData);
+        }
+        return outData;
     }
 
     @Override
